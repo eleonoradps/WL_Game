b4fe6bf4989da089e016ea709f6dfc6a673a9d5c
L <eleonora.dups@gmail.com>
2020-02-11 17:23:54 +0100

    fixed bug in shape container activator script

---
+++ Assets/Scripts/ShapeContainerActivator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeContainerActivator : MonoBehaviour
+{
+    [SerializeField] private GameObject shapeContainerP1;
+    [SerializeField] private GameObject shapecontainerP2;
+
+    private bool isShapeContainerP1Active = false;
+    private bool isShapeContainerP2Active = false;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public void ActivateP1ShapeContainer()
+    {
+        isShapeContainerP1Active = !isShapeContainerP1Active;
+        shapeContainerP1.SetActive(isShapeContainerP1Active);
+    }
+
+    public void ActivateP2ShapeContainer()
+    {
+        isShapeContainerP2Active = !isShapeContainerP2Active;
+        shapecontainerP2.SetActive(true);
+    }
+}
