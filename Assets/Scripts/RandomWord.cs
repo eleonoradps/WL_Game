b53a67362824b1afb577444637dd1403a7b4d1b6
William PEPIN <williamp10022000@gmail.com>
2020-02-11 17:01:20 +0100

    added random word to draw

---
+++ Assets/Scripts/RandomWord.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RandomWord : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> words = new List<string>();
+
+    int wordIndex;
+    
+    [SerializeField] TextMeshProUGUI wordText;
+    
+    void Start()
+    {
+        wordIndex = Random.Range (0,(words.Count));
+        wordText.text = words[wordIndex];
+    }
+
+ 
+}
