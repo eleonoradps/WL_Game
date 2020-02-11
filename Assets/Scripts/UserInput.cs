853a7150afc10774a4a1c957ced256f1b00a4d5b
William PEPIN <williamp10022000@gmail.com>
2020-02-11 17:43:28 +0100

    added user input

---
+++ Assets/Scripts/UserInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.UI;
+
+public class UserInput : MonoBehaviour
+{
+    [SerializeField] TMP_InputField userInput;
+    string userWord;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            userWord = userInput.text;
+            Debug.Log(userWord);
+            userInput.text = null;
+        }
+        
+    }
+}
