919cdb7da505aaa4ddd5e456c264dfe1728e5789
L <eleonora.dups@gmail.com>
2020-02-11 18:48:49 +0100

    changed button sprite and started drag and drop script

---
+++ Assets/Scripts/DragAndDropShape.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDropShape : MonoBehaviour
+{
+
+    [SerializeField] GameObject squarePrefab;
+    [SerializeField] GameObject circlePrefab;
+    [SerializeField] GameObject trianglePrefab;
+    [SerializeField] GameObject linePrefab;
+
+    public void InstantiateSquare()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(squarePrefab);
+        }
+    }
+
+    public void InstantiateCircle()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(circlePrefab);
+        }
+    }
+
+    public void InstantiateTriangle()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(trianglePrefab);
+        }
+    }
+
+    public void InstantiateLine()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(linePrefab);
+        }
+    }
+}
