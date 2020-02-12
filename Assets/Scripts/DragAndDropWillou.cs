06c90fd75ea7d7dde36ce46ff78f88c147d0d539
William PEPIN <williamp10022000@gmail.com>
2020-02-12 18:04:57 +0100

    change game scene design

---
+++ Assets/Scripts/DragAndDropWillou.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDropWillou : MonoBehaviour, IDragHandler 
+{
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+        transform.position = Input.mousePosition;
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    
+}
